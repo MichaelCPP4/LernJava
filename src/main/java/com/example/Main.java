2635146cbe9e4bd70cd0d4512770cb8f8c3c1296
MichaelCPP4 <michaelplay3452@gmail.com>
2025-10-26 19:00:14 +0300

    com2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,12 +1,6 @@
 package com.example;
 
-import com.example.dao.UserDao;
-import com.example.entity.User;
 import com.example.service.UserMenu;
-
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
 
 /**
  * Консольное CRUD-приложение для управления пользователями.
